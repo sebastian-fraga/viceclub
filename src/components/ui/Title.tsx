e385a15308a4852c1071070814efa26a29507ea3
seba <63276982+sebaa1@users.noreply.github.com>
2026-08-27 14:18:42 -0300

    refactor(title): add optional prop to component

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -1,15 +1,17 @@
 import { useTranslation } from "react-i18next";
+import type { TOptions } from "i18next";
 
 interface Props {
     label: string;
+    options?: TOptions;
 }
 
-export default function Title({ label }: Props) {
+export default function Title({ label, options }: Props) {
     const { t } = useTranslation();
 
     return (
         <h2 className="text-5xl max-mobile:text-3xl text-white uppercase font-bold tracking-wide relative mb-2 max-mobile:max-w-prose text-pretty font-body-condensed">
-            {t(label)}
+            {t(label, options)}
             <div className="absolute w-full h-px top-full mt-1 bg-pink-500"></div>
         </h2>
     );
