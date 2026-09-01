5e4bc309426f96df17d8248a9977a31e0e8c4d6a
seba <63276982+sebaa1@users.noreply.github.com>
2026-09-01 16:40:23 -0300

    feat: update app version

--- src/lib/app/version.ts
+++ src/lib/app/version.ts
@@ -1,4 +1,4 @@
 import { execSync } from "node:child_process";
 
-export const APP_VERSION = "2.0.0";
+export const APP_VERSION = "2.1.0";
 export const COMMIT = execSync("git rev-parse --short HEAD").toString().trim();
