1f805bb2ac9bf94f460f0c73e1358e1f45794467
seba <63276982+sebaa1@users.noreply.github.com>
2026-09-01 23:42:16 -0300

    feat: add alignment option to Title component

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -1,18 +1,28 @@
-import type { TOptions } from "i18next";
 import useT from "@/hooks/useT";
+import type { TOptions } from "i18next";
 
 interface Props {
     label: string;
     options?: TOptions;
+    align?: "center" | "left";
 }
 
-export default function Title({ label, options }: Props) {
+export default function Title({ label, options, align = "center" }: Props) {
     const t = useT();
+    const isCentered = align === "center";
 
     return (
-        <h2 className="text-5xl max-mobile:text-3xl text-white uppercase font-bold tracking-wide relative mb-2 max-mobile:max-w-prose text-pretty font-body-condensed">
+        <h2
+            className={`text-5xl max-mobile:text-4xl/10 text-white uppercase font-bold tracking-wide mb-2 font-body-condensed text-pretty ${
+                isCentered ? "text-center max-mobile:px-12" : "text-left"
+            }`}
+        >
             {t(label, options)}
-            <div className="absolute w-full h-px top-full mt-1 bg-pink-500"></div>
+            <div
+                className={`w-22 h-px bg-pink-500 mt-2 ${
+                    isCentered ? "max-mobile:mx-auto" : ""
+                }`}
+            ></div>
         </h2>
     );
 }
