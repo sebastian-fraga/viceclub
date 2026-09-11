2dfd3c83aa02759f5d86193c1263a55ca7e0c4e4
seba <63276982+sebaa1@users.noreply.github.com>
2026-09-11 14:44:23 -0300

    feat(maps): add VC markers to index

--- src/data/maps/markers/index.ts
+++ src/data/maps/markers/index.ts
@@ -1,7 +1,9 @@
 import { III_MARKERS } from "./III";
 import { SA_MARKERS } from "./SA";
+import { VC_MARKERS } from "./VC";
 
 export const MAP_MARKERS = {
     III: III_MARKERS,
+    VC: VC_MARKERS,
     SA: SA_MARKERS,
 } as const;
