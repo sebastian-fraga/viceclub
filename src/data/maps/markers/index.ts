ead7f1642609f7b94551e47913e7366627d730b7
seba <63276982+sebaa1@users.noreply.github.com>
2026-09-21 22:31:42 -0300

    feat(maps): add GTA IV markers to index file

--- src/data/maps/markers/index.ts
+++ src/data/maps/markers/index.ts
@@ -3,6 +3,7 @@ import { LCS_MARKERS } from "./LCS";
 import { SA_MARKERS } from "./SA";
 import { VC_MARKERS } from "./VC";
 import { VCS_MARKERS } from "./VCS";
+import { IV_MARKERS } from "./IV";
 
 export const MAP_MARKERS = {
     III: III_MARKERS,
@@ -10,4 +11,5 @@ export const MAP_MARKERS = {
     SA: SA_MARKERS,
     LCS: LCS_MARKERS,
     VCS: VCS_MARKERS,
+    IV: IV_MARKERS,
 } as const;
